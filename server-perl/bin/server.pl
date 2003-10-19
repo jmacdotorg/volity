2e0c867d91c1c78d833d3984b3d21616a0a77a3f
Jason McIntosh <jmac@jmac.org>
2003-10-19 17:06:23 +0000

    It now specifies referee class instead of a game class, reflecting changes in the Frivolity libraries. Also, you must now pass in GPG information.

--- server-perl/bin/server.pl
+++ server-perl/bin/server.pl
@@ -5,15 +5,30 @@ use strict;
 
 use Volity::Server;
 use Getopt::Std;
+use Volity::GameRecord;
 
 my %opts;
-getopts('u:p:h:r:d:g:o:', \%opts);
+getopts('u:p:h:r:d:g:o:b:G:K:P:', \%opts);
 
-foreach ('user', 'host', 'password', 'resource', 'game class') {
+foreach ('user', 'host', 'password', 'resource', 'bookkeeper_jid') {
   unless (defined($opts{substr($_, 0, 1)})) {
     die "You must define a $_, with the " . substr($_, 0, 1) . " switch.\n";
   }
 }
+
+foreach ('key id (GPG)', 'passphrase (GPG)', 'GPG binary path',) {
+  unless (defined($opts{uc(substr($_, 0, 1))})) {
+    die "You must define a $_, with the " . uc(substr($_, 0, 1)) . " switch.\n";
+  }
+}
+
+
+unless (defined($opts{substr('g', 0, 1)})) {
+  die "You must define a referee class, with the g switch\n";
+}
+
+# Hardcoded default bookkeeper JID.
+my $default_bkp = 'bookkeeper@volity.com';
 
 my $server = Volity::Server->new(
 				 {
@@ -24,7 +39,12 @@ my $server = Volity::Server->new(
 				  resource=>$opts{r},
 				  debug=>$opts{d} || 0,
 				  alias=>'volity',
-				  game_class=>$opts{g},
+				  referee_class=>$opts{g},
+				  bookkeeper_jid=>$opts{b} || $default_bkp,
 				}
 				);
+$Volity::GameRecord::gpg_bin = $opts{G};
+$Volity::GameRecord::gpg_secretkey = $opts{K};
+$Volity::GameRecord::gpg_passphrase = $opts{P};
+
 $server->start;
