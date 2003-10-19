cad0ee8b01fb035b833b47c4e6f565e21a10b6c7
Jason McIntosh <jmac@jmac.org>
2003-10-19 17:27:16 +0000

    Added a 'basic_jid' convenience method.

--- server-perl/lib/Volity/Player.pm
+++ server-perl/lib/Volity/Player.pm
@@ -6,6 +6,16 @@ use strict;
 use base qw(Class::Accessor::Fields);
 use fields qw(jid name nick);
 
-Volity::Player->create_accessors;
+#Volity::Player->create_accessors;
+
+# basic_jid: Return the non-resource part of my JID.
+sub basic_jid {
+  my $self = shift;
+  if (defined($self->jid) and $self->jid =~ /^(.*)\//) {
+    return $1;
+  }
+  return undef;
+}
+
 
 1;
