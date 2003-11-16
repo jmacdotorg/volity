e6c70092018517272c95daf036c9cf6f7614d5d2
Jason McIntosh <jmac@jmac.org>
2003-11-16 01:35:10 +0000

    Totally redid the info modules to use Class::DBI. Also, got rid of separate refs to 'first' and 'last' player names, preferring a single 'name' field.

--- bookkeeper-perl/lib/Volity/Info/Game.pm
+++ bookkeeper-perl/lib/Volity/Info/Game.pm
@@ -22,101 +22,22 @@ use warnings;
 use strict;
 
 use base qw(Volity::Info);
-use fields qw(start_time end_time server_jid signature uri players winners quitters name description);
 
-use Data::Lazy;
-use Carp qw(carp croak);
+Volity::Info::Game->table('game');
+Volity::Info::Game->columns(All=>qw(id start_time end_time server_jid signature uri));
+Volity::Info::Game->has_a(uri=>"Volity::Info::URI");
+Volity::Info::Game->has_many(players=>["Volity::Info::GamePlayer" => 'player_jid'], 'game_id');
+Volity::Info::Game->has_many(winners=>["Volity::Info::GameWinner" => 'player_jid'], 'game_id');
+Volity::Info::Game->has_many(quitters=>["Volity::Info::GameQuitter" => 'player_jid'], 'game_id');
 
-our %known_games;
-
-sub initialize {
+sub name {
   my $self = shift;
-  $self->SUPER::initialize(@_);
-  foreach (qw(players winners quitters)) {
-    my $method = "fetch_${_}_from_db";
-    tie ($self->{$_}, "Data::Lazy", sub {$self->$method});
-  }
+  return $self->uri->name;
 }
 
-# XXX Ew... refactor these methods, please.
-sub fetch_players_from_db {
+sub description {
   my $self = shift;
-  my $dbh = $self->dbh->clone;
-  $dbh->select({fields=>'PLAYER.JID',
-		table=>'GAME, PLAYER, GAME_PLAYER', 
-		where=>{'GAME.ID'=>$self->id,},
-		join=>['GAME_PLAYER.GAME_ID = GAME.ID',
-		       'GAME_PLAYER.PLAYER_JID = PLAYER.JID',
-		      ],
-	      });
-  my @players;
-  while (my ($player_id) = $dbh->fetchrow_array) {
-    my $player = Volity::Info::Player->new({jid=>$player_id});
-    push (@players, $player);
-  }
-  $self->players(@players);
-  return $self->{players};
-}
-
-sub fetch_winners_from_db {
-  my $self = shift;
-  my $dbh = $self->dbh->clone;
-  $dbh->opt(saveSQL=>1);
-  $dbh->select({fields=>'PLAYER_JID',
-		table=>'GAME, GAME_WINNER', 
-		where=>{'GAME.ID'=>$self->id,},
-		join=>['GAME_WINNER.GAME_ID = GAME.ID',
-		      ],
-	      });
-  my @players;
-  while (my ($player_id) = $dbh->fetchrow_array) {
-    my $player = Volity::Info::Player->new({jid=>$player_id});
-    push (@players, $player);
-  }
-  $self->winners(@players);
-  return $self->{winners};
-}
-
-sub fetch_quitters_from_db {
-  my $self = shift;
-  my $dbh = $self->dbh->clone;
-  $dbh->select({fields=>'PLAYER_JID',
-		table=>'GAME, GAME_QUITTER', 
-		where=>{'GAME.ID'=>$self->id,},
-		join=>['GAME_QUITTER.GAME_ID = GAME.ID',
-		      ],
-	      });
-  my @players;
-  while (my ($player_id) = $dbh->fetchrow_array) {
-    my $player = Volity::Info::Player->new({jid=>$player_id});
-    push (@players, $player);
-  }
-  $self->quitters(@players);
-  return $self->{quitters};
-}
-
-sub fetch_attributes_from_db {
-  my $self = shift;
-  my $dbh = $self->dbh;
-  $dbh->select('*', 'GAME', {ID=>$self->id});
-  my $data = $dbh->fetchrow_hashref;
-  foreach (keys(%$data)) {
-    $self->$_($$data{$_});
-  }
-
-  # Fetch the game's name from the URI table, if we know this game's URI...
-  if (defined($self->uri)) {
-    $dbh->select('NAME, DESCRIPTION', 'URI', {uri=>$self->uri});
-    my $data = $dbh->fetchrow_hashref;
-    foreach (keys(%$data)) {
-      my $method = lc($_);
-      $self->$method($$data{$_});
-    }
-  }
-}
-
-sub known_object_hashref {
-  return \%known_games;
+  return $self->uri->description;
 }
 
 1;
