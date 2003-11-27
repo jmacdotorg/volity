6e4412c80ed5f1d3db8f457d7d1636a8b7304237
Jason McIntosh <jmac@jmac.org>
2003-11-27 02:20:18 +0000

    Incremented VERSION, put in silly non-docs

--- server-perl/lib/Volity.pm
+++ server-perl/lib/Volity.pm
@@ -1,10 +1,24 @@
 package Volity;
 
-our $VERSION = '0.2';
+our $VERSION = '0.2.1';
 
 =pod
 
 I have no docs! Oh no!
+
+=begin todo-docs
+
+Things to document
+
+* What dis?
+
+* How to use this system, for game designers, and game runners. (i.e. which man pages to read)
+
+* What is a "JID"?
+
+* Object constructors and accessors
+
+=end
 
 =cut
 
