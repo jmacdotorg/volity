589f5db1a66d5d27c50de5796e4e0499f315308b
Jason McIntosh <jmac@jmac.org>
2004-02-13 04:28:12 +0000

    Bugfix, and some class info (hmm, maybe user and host shouldn't be there...

---
+++ text-games-perl/lib/Bot/RPS.pm
@@ -0,0 +1,40 @@
+package Volity::Bot::RPS;
+
+# This is a superclass for bots that can play rock-paper-scissors.
+
+use warnings; use strict;
+
+use base qw(Volity::Bot);
+# Mmm, take note of which fields should go in a bot class.
+use fields qw(has_guessed);
+
+use POE;
+
+__PACKAGE__->name("RandomBot");
+__PACKAGE__->description("Generic RPS-playing bot.");
+__PACKAGE__->user("rps-bot");
+__PACKAGE__->host("volity.net");
+
+sub take_turn {
+  my $self = shift;
+  my @choices = qw(rock paper scissors);
+  my $hand = $choices[rand(@choices)];
+  $self->send_message({
+		       type=>'chat',
+		       to=>$self->referee->jid,
+		       body=>$hand,
+		     });
+}
+
+sub handle_groupchat_message {
+  my $self = shift;
+  my ($message) = @_;
+#  warn "Got a message: $$message{body}\n";
+  if ($$message{body} =~ /game has begun!/) {
+#    warn "OK, I am " . $self->nickname . " and I'm taking my turn.\n";
+    $self->take_turn;
+  }
+
+}
+
+1;
