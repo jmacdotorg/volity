b88a38babcfb0ee813930d11525c9507d03deb92
Jason McIntosh <jmac@jmac.org>
2004-03-14 19:06:13 +0000

    Added bot support and PID-file support

--- server-perl/bin/server.pl
+++ server-perl/bin/server.pl
@@ -11,9 +11,9 @@ use Getopt::Std;
 use Volity::GameRecord;
 
 my %opts;
-getopts('u:p:h:r:d:g:o:b:G:K:P:R:', \%opts);
+getopts('u:p:h:r:d:g:o:b:G:K:P:R:m:B:f:', \%opts);
 
-foreach ('user', 'host', 'password', 'resource', 'bookkeeper_jid') {
+foreach ('user', 'host', 'password',) {
   unless (defined($opts{substr($_, 0, 1)})) {
     die "You must define a $_, with the " . substr($_, 0, 1) . " switch.\n";
   }
@@ -28,6 +28,26 @@ foreach ('key id (GPG)', 'passphrase (GPG)', 'GPG binary path',) {
 
 unless (defined($opts{g}) or defined($opts{R})) {
   die "You must define either a game class (-g) or a referee class (-R).\n";
+}
+
+if (defined($opts{f})) {
+  open (PID, ">$opts{f}") or die "Can't write a PIDfile to $opts{f}: $!";
+  print PID $$;
+  close PID or die "Can't close PIDfile $opts{f}: $!";
+}
+
+# Parse the bot list, if present...
+my @bot_classes;
+if (defined($opts{B})) {
+    @bot_classes = split(/\s*,\s*/, $opts{B});
+    foreach (@bot_classes) {
+	eval"(require $_);";
+	if ($@) {
+	    die "Can't use bot class $_: $@\n";
+	}
+    }
+} else {
+    @bot_classes = ();
 }
 
 # Hardcoded default bookkeeper JID.
@@ -48,10 +68,11 @@ my $server = Volity::Server->new(
 				  game_class=>$opts{g},
 				  referee_class=>$opts{R},
 				  bookkeeper_jid=>$opts{b} || $default_bkp,
+				  muc_host=>$opts{m} || 'conference.volity.net',
 				}
 				);
+$server->bot_classes(@bot_classes);
 $Volity::GameRecord::gpg_bin = $opts{G};
 $Volity::GameRecord::gpg_secretkey = $opts{K};
 $Volity::GameRecord::gpg_passphrase = $opts{P};
-
 $server->start;
