6558908025992ad61cf796543c5f0b53cfc84799
Doug Oreleans <dougorleans@gmail.com>
2004-04-20 07:39:03 +0000

    Add myNickname and opponents JavaScript objects. Wrap List and Map objects into Scriptables. Call Context.enter()/exit() in each thread.

--- Javolin/source/org/volity/client/GameUI.java
+++ Javolin/source/org/volity/client/GameUI.java
@@ -6,7 +6,9 @@ import java.util.*;
 import org.mozilla.javascript.*;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.util.StringUtils;
 import org.volity.jabber.*;
+import org.volity.jabber.packet.MUCUser;
 
 /**
  * A game user interface.  Subclasses must implement writeln, endGame,
@@ -25,8 +27,8 @@ public abstract class GameUI extends RPCResponder {
 
   GameTable table;
   File uiScript;
-  Context context;
   Scriptable scope, game, commandFunctions;
+  RPCWrapFactory rpcWrapFactory = new RPCWrapFactory();
 
   /**
    * Execute the UI script.
@@ -34,39 +36,60 @@ public abstract class GameUI extends RPCResponder {
    * @throws JavaScriptException if the UI script causes an exception
    */
   public void start() throws IOException, JavaScriptException {
-    context = Context.enter();
-    scope = context.initStandardObjects();
-    scope.put("game", scope, game = context.newObject(scope));
-    // FIXME: referee is a misleading name for this object
-    scope.put("referee", scope, commandFunctions = context.newObject(scope));
+    try {
+      Context context = Context.enter();
+      scope = context.initStandardObjects();
 
-    scope.put("writeln", scope, new Callback() {
-	public Object run(Object[] args) {
-	  writeln((String) args[0]);
-	  return null;
-	}
-      });
-    scope.put("rpc", scope, new Callback() {
-	public Object run(Object[] args) {
-	  try {
-	    List params = Arrays.asList(args).subList(1, args.length);
-	    return table.getReferee().invoke("game." + args[0], params);
-	  } catch (Exception e) {
-	    error(e);
+      scope.put("game", scope, game = context.newObject(scope));
+      game.put("myNickname", game, table.getNickname());
+      Scriptable opponents = context.newObject(scope);
+      for (Iterator it = table.getParticipants(); it.hasNext();) {
+	String roomJID = (String) it.next();
+	MUCUser userInfo = table.getUserInfo(roomJID);
+	if (userInfo.getAffiliation() == MUCUser.Affiliation.OWNER)
+	  // Skip the ref.
+	  continue;
+	String nickname = StringUtils.parseResource(roomJID);
+	if (nickname.equals(table.getNickname()))
+	  // Skip myself.
+	  continue;
+	opponents.put(nickname, opponents, context.newObject(scope));
+      }
+      game.put("opponents", game, opponents);
+
+      // FIXME: referee is a misleading name for this object
+      scope.put("referee", scope, commandFunctions = context.newObject(scope));
+
+      scope.put("writeln", scope, new Callback() {
+	  public Object run(Object[] args) {
+	    writeln(String.valueOf(args[0]));
 	    return null;
 	  }
-	}
-      });
-    commandFunctions.put("registerCommand", commandFunctions, new Callback() {
-	public Object run(Object[] args) {
-	  commands.add(new Command((String) args[0], (String) args[1],
-				   (String) args[2]));
-	  return null;
-	}
-      });
+	});
+      scope.put("rpc", scope, new Callback() {
+	  public Object run(Object[] args) {
+	    try {
+	      List params = Arrays.asList(args).subList(1, args.length);
+	      return table.getReferee().invoke("game." + args[0], params);
+	    } catch (Exception e) {
+	      error(e);
+	      return null;
+	    }
+	  }
+	});
+      commandFunctions.put("registerCommand", commandFunctions, new Callback() {
+	  public Object run(Object[] args) {
+	    commands.add(new Command((String) args[0], (String) args[1],
+				     (String) args[2]));
+	    return null;
+	  }
+	});
 
-    context.evaluateReader(scope, new FileReader(uiScript),
-			   uiScript.getName(), 1, null);
+      context.evaluateReader(scope, new FileReader(uiScript),
+			     uiScript.getName(), 1, null);
+    } finally {
+      Context.exit();
+    }
   }
 
   // Inherited from RPCResponder
@@ -75,6 +98,8 @@ public abstract class GameUI extends RPCResponder {
       Object method = game.get(methodName.substring(5), scope);
       if (method instanceof Function)
 	try {
+	  Context context = Context.enter();
+	  context.setWrapFactory(rpcWrapFactory);
 	  Object ret = ((Function) method).call(context, scope, game,
 						params.toArray());
 	  if (ret instanceof Undefined) {
@@ -86,6 +111,8 @@ public abstract class GameUI extends RPCResponder {
 	  error(e);
 	  // FIXME: Volity protocol should probably define these error codes.
 	  throw new RPCException(901, "UI script error: " + e);
+	} finally {
+	  Context.exit();
 	}
       else
 	throw new RPCException(902, "No such UI function.");
@@ -94,6 +121,145 @@ public abstract class GameUI extends RPCResponder {
       return Boolean.TRUE;
     } else
       throw new RPCException(903, "No such client method.");
+  }
+
+  /**
+   * A factory for wrapping RPC data types into JavaScript objects.
+   * In particular, RPC arrays and structs are List objects and Map
+   * objects, respectively, and this turns them both into Scriptables.
+   */
+  class RPCWrapFactory extends WrapFactory {
+    public Object wrap(Context cs, Scriptable scope,
+		       Object obj, Class staticType)
+    {
+      if (obj != null && obj instanceof List) {
+	final List list = (List) obj;
+	return new Scriptable() {
+	    public boolean has(int index, Scriptable start) {
+	      return index >= 0 && index < list.size();
+	    }
+	    public Object get(int index, Scriptable start) {
+	      return list.get(index);
+	    }
+	    public void put(int index, Scriptable start, Object value) {
+	      list.set(index, value);
+	    }
+	    public void delete(int index) {
+	      list.remove(index);
+	    }
+	    public Object[] getIds() {
+	      Object[] ids = new Object[list.size()];
+	      for (int i = 0; i < ids.length; i++)
+		ids[i] = new Integer(i);
+	      return ids;
+	    }
+
+	    // Not really sure what to do with all these...
+	    public String getClassName() {
+	      return list.getClass().getName();
+	    }
+	    public Object getDefaultValue(Class hint) {
+	      if (hint == null || hint == String.class)
+		return list.toString();
+	      if (hint == Boolean.class)
+		return Boolean.TRUE;
+	      if (hint == Number.class)
+		return new Double(Double.NaN);
+	      return this;
+	    }
+	    public boolean hasInstance(Scriptable instance) {
+	      return false;
+	    }
+	    public boolean has(String name, Scriptable start) {
+	      return false;
+	    }
+	    public Object get(String name, Scriptable start) {
+	      return NOT_FOUND;
+	    }
+	    public void put(String name, Scriptable start, Object value) {
+	      // ignore
+	    }
+	    public void delete(String name) {
+	      // ignore
+	    }
+	    public Scriptable getParentScope() {
+	      return null;
+	    }
+	    public void setParentScope(Scriptable parent) {
+	      // ignore
+	    }
+	    public Scriptable getPrototype() {
+	      return null;
+	    }
+	    public void setPrototype(Scriptable prototype) {
+	      // ignore;
+	    }
+	  };
+      } else if (obj != null && obj instanceof Map) {
+	final Map map = (Map) obj;
+	return new Scriptable() {
+	    public boolean has(String name, Scriptable start) {
+	      return map.containsKey(name);
+	    }
+	    public Object get(String name, Scriptable start) {
+	      return map.get(name);
+	    }
+	    public void put(String name, Scriptable start, Object value) {
+	      map.put(name, value);
+	    }
+	    public void delete(String name) {
+	      map.remove(name);
+	    }
+
+	    public Object[] getIds() {
+	      return map.keySet().toArray();
+	    }
+
+	    // Not really sure what to do with all these...
+	    public String getClassName() {
+	      return map.getClass().getName();
+	    }
+	    public Object getDefaultValue(Class hint) {
+	      if (hint == null || hint == String.class)
+		return map.toString();
+	      if (hint == Boolean.class)
+		return Boolean.TRUE;
+	      if (hint == Number.class)
+		return new Double(Double.NaN);
+	      return this;
+	    }
+	    public boolean hasInstance(Scriptable instance) {
+	      return false;
+	    }
+	    public boolean has(int index, Scriptable start) {
+	      return false;
+	    }
+	    public Object get(int index, Scriptable start) {
+	      return NOT_FOUND;
+	    }
+	    public void put(int index, Scriptable start, Object value) {
+	      // ignore
+	    }
+	    public void delete(int index) {
+	      // ignore
+	    }
+	    public Scriptable getParentScope() {
+	      return null;
+	    }
+	    public void setParentScope(Scriptable parent) {
+	      // ignore
+	    }
+	    public Scriptable getPrototype() {
+	      return null;
+	    }
+	    public void setPrototype(Scriptable prototype) {
+	      // ignore;
+	    }
+	  };
+      } else {
+	return super.wrap(cs, scope, obj, staticType);
+      }
+    }
   }
 
   /** An unmodifiable list of the registered commands (Command objects). */
@@ -149,8 +315,13 @@ public abstract class GameUI extends RPCResponder {
     public Object invoke(String args)
       throws NoSuchMethodException, JavaScriptException
     {
-      return getFunction().call(context, scope, commandFunctions,
-				new Object[] { args });
+      try {
+	Context context = Context.enter();
+	return getFunction().call(context, scope, commandFunctions,
+				  new Object[] { args });
+      } finally {
+	Context.exit();
+      }
     }
   }
 
