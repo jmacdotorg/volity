3cf362622ac6517aa50671afca250e9bb48a957b
Doug Oreleans <dougorleans@gmail.com>
2004-07-24 21:06:01 +0000

    Moved Friv-specific stuff into FrivUI. Made info.nickname a dynamic property. Initialize info.opponents as late as possible.

--- Javolin/source/org/volity/client/GameUI.java
+++ Javolin/source/org/volity/client/GameUI.java
@@ -1,126 +1,150 @@
 package org.volity.client;
 
 import java.io.*;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.*;
 import org.mozilla.javascript.*;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.util.StringUtils;
+import org.jivesoftware.smackx.packet.MUCUser;
 import org.volity.jabber.*;
-import org.volity.jabber.packet.MUCUser;
 
 /**
- * A game user interface.  Subclasses must implement writeln, endGame,
- * and error.
+ * A game user interface.
  */
-public abstract class GameUI extends RPCResponder {
+public class GameUI extends RPCResponder implements RPCHandler {
   /**
-   * @param table the table where the game will be played
-   * @param uiScript an ECMAScript file implementing the game UI
+   * @param connection an authenticated connection to an XMPP server
+   * @param errorHandler a handler for UI script and RPC errors
+   * @throws IllegalStateException if the connection has not been authenticated
    */
-  public GameUI(GameTable table, File uiScript) {
-    super(table.getConnection());
-    this.table = table;
-    this.uiScript = uiScript;
+  public GameUI(XMPPConnection connection, ErrorHandler errorHandler) {
+    super(connection, new RPCDispatcher());
+    this.errorHandler = errorHandler;
+    initStandardObjects();
+    start();
   }
 
+  ErrorHandler errorHandler;
+  Scriptable scope, game, info, client;
   GameTable table;
-  File uiScript;
-  Scriptable scope, game, commandFunctions;
   RPCWrapFactory rpcWrapFactory = new RPCWrapFactory();
 
-  /**
-   * Execute the UI script.
-   * @throws IOException if there is an I/O problem reading the UI script
-   * @throws JavaScriptException if the UI script causes an exception
-   */
-  public void start() throws IOException, JavaScriptException {
+  public interface ErrorHandler {
+    /**
+     * Report a command error.
+     */
+    public abstract void error(Exception e);
+  }
+
+  protected void initStandardObjects() {
     try {
       Context context = Context.enter();
       scope = context.initStandardObjects();
-
       scope.put("game", scope, game = context.newObject(scope));
-      game.put("myNickname", game, table.getNickname());
-      Scriptable opponents = context.newObject(scope);
-      for (Iterator it = table.getParticipants(); it.hasNext();) {
-	String roomJID = (String) it.next();
-	MUCUser userInfo = table.getUserInfo(roomJID);
-	if (userInfo.getAffiliation() == MUCUser.Affiliation.OWNER)
-	  // Skip the ref.
-	  continue;
-	String nickname = StringUtils.parseResource(roomJID);
-	if (nickname.equals(table.getNickname()))
-	  // Skip myself.
-	  continue;
-	opponents.put(nickname, opponents, context.newObject(scope));
-      }
-      game.put("opponents", game, opponents);
-
-      // FIXME: referee is a misleading name for this object
-      scope.put("referee", scope, commandFunctions = context.newObject(scope));
-
-      scope.put("writeln", scope, new Callback() {
-	  public Object run(Object[] args) {
-	    writeln(String.valueOf(args[0]));
-	    return null;
-	  }
-	});
+      scope.put("info", scope, info = new Info());
+      scope.put("client", scope, client = context.newObject(scope));
       scope.put("rpc", scope, new Callback() {
 	  public Object run(Object[] args) {
 	    try {
 	      List params = Arrays.asList(args).subList(1, args.length);
 	      return table.getReferee().invoke("game." + args[0], params);
 	    } catch (Exception e) {
-	      error(e);
+	      errorHandler.error(e);
 	      return null;
 	    }
 	  }
 	});
-      commandFunctions.put("registerCommand", commandFunctions, new Callback() {
-	  public Object run(Object[] args) {
-	    commands.add(new Command((String) args[0], (String) args[1],
-				     (String) args[2]));
-	    return null;
-	  }
-	});
-
-      context.evaluateReader(scope, new FileReader(uiScript),
-			     uiScript.getName(), 1, null);
+      RPCDispatcher dispatcher = (RPCDispatcher) getHandler();
+      dispatcher.setHandler("game", this);
+    } catch (JavaScriptException e) {
+      errorHandler.error(e);
     } finally {
       Context.exit();
     }
   }
 
-  // Inherited from RPCResponder
-  public Object handleRPC(String methodName, List params) throws RPCException {
-    if (methodName.startsWith("game.")) {
-      Object method = game.get(methodName.substring(5), scope);
-      if (method instanceof Function)
-	try {
-	  Context context = Context.enter();
-	  context.setWrapFactory(rpcWrapFactory);
-	  Object ret = ((Function) method).call(context, scope, game,
-						params.toArray());
-	  if (ret instanceof Undefined) {
-	    // function returned void, but RPC result has to be non-void
-	    ret = Boolean.TRUE;
-	  }
-	  return ret;
-	} catch (JavaScriptException e) {
-	  error(e);
-	  // FIXME: Volity protocol should probably define these error codes.
-	  throw new RPCException(901, "UI script error: " + e);
-	} finally {
-	  Context.exit();
+  class Info extends ScriptableObject {
+    public String getClassName() { return "Info"; }
+    {
+      try {
+	defineProperty("nickname", Info.class, PERMANENT);
+	defineProperty("opponents", Info.class, PERMANENT);
+      } catch (PropertyException e) {
+	throw new RuntimeException(e.toString());
+      }
+    }
+    public String getNickname() { return table.getNickname(); }
+    public void setNickname(String nickname) throws XMPPException {
+      table.changeNickname(nickname);
+    }
+    Scriptable opponents;
+    public Object getOpponents() throws JavaScriptException {
+      if (opponents == null) {
+	Context context = Context.getCurrentContext();
+	opponents = context.newObject(scope);
+	for (Iterator it = table.getOpponents().iterator(); it.hasNext();) {
+	  String nickname = (String) it.next();
+	  opponents.put(nickname, opponents, context.newObject(scope));
 	}
-      else
-	throw new RPCException(902, "No such UI function.");
-    } else if (methodName.equals("end_game")) {
-      endGame();
-      return Boolean.TRUE;
-    } else
-      throw new RPCException(903, "No such client method.");
+      }
+      return opponents;
+    }
+  }
+
+  /**
+   * @param table the table where the game will be played
+   */
+  public void setTable(GameTable table) {
+    this.table = table;
+  }
+  
+  /**
+   * Execute a UI script.
+   * @param uiScript an ECMAScript file implementing the game UI
+   * @throws IOException if there is an I/O problem reading the UI script
+   * @throws JavaScriptException if the UI script throws an exception
+   * @throws EvaluationException if an error occurs while evaluating
+   *                             the UI script
+   */
+  public void load(File uiScript) throws IOException, JavaScriptException {
+    try {
+      Context.enter().evaluateReader(scope, new FileReader(uiScript),
+				     uiScript.getName(), 1, null);
+    } finally {
+      Context.exit();
+    }
+  }
+
+  // Inherited from RPCHandler.
+  public Object handleRPC(String methodName, List params)
+    throws RPCException
+  {
+    Object method = game.get(methodName, scope);
+    if (method instanceof Function)
+      try {
+	Context context = Context.enter();
+	context.setWrapFactory(rpcWrapFactory);
+	Object ret = ((Function) method).call(context, scope, game,
+					      params.toArray());
+	if (ret instanceof Undefined) {
+	  // function returned void, but RPC result has to be non-void
+	  ret = Boolean.TRUE;
+	}
+	return ret;
+      } catch (JavaScriptException e) {
+	errorHandler.error(e);
+	// FIXME: Volity protocol should probably define these
+	// error codes.
+	throw new RPCException(901, "UI script exception: " + e);
+      } catch (EvaluatorException e) {
+	errorHandler.error(e);
+	throw new RPCException(902, "UI script error: " + e);
+      } finally {
+	Context.exit();
+      }
+    else
+      throw new RPCException(903, "No such UI function.");
   }
 
   /**
@@ -262,69 +286,6 @@ public abstract class GameUI extends RPCResponder {
     }
   }
 
-  /** An unmodifiable list of the registered commands (Command objects). */
-  public List getCommands() { return Collections.unmodifiableList(commands); }
-  List commands = new ArrayList();
-
-  /** A user command, registered by the UI script and invoked by the client. */
-  public class Command {
-    /**
-     * @param name the name of the command
-     * @param shortDescription a short description of the command
-     * @param longDescription a multi-line description of the command,
-     *                        for help text
-     */
-    public Command(String name, String shortDescription,
-		   String longDescription)
-    {
-      this.name = name;
-      this.shortDescription = shortDescription;
-      this.longDescription = longDescription;
-    }
-
-    String name;
-    public String getName() { return name; }
-
-    String shortDescription;
-    public String getShortDescription() { return shortDescription; }
-
-    String longDescription;
-    public String getLongDescription() { return longDescription; }
-
-    Function function;
-    // We can't set this at construction time (when registerCommand is
-    // invoked) because the script is still being evaluated and the
-    // command might not have been defined yet!
-    Function getFunction() throws NoSuchMethodException {
-      if (function == null) {
-	Object f = commandFunctions.get(name, commandFunctions);
-	if (!(f instanceof Function))
-	  throw new NoSuchMethodException(name);
-	function = (Function) f;
-      }
-      return function;
-    }
-
-    /**
-     * Invoke this command, which is passed to the UI script.
-     * @param args the arguments to the command
-     * @return the return value from the command
-     * @throws NoSuchMethodException if the command is undefined
-     * @throws JavaScriptException if the command causes an exception
-     */
-    public Object invoke(String args)
-      throws NoSuchMethodException, JavaScriptException
-    {
-      try {
-	Context context = Context.enter();
-	return getFunction().call(context, scope, commandFunctions,
-				  new Object[] { args });
-      } finally {
-	Context.exit();
-      }
-    }
-  }
-
   /** A simple way to define a function object without using reflection. */
   abstract class Callback extends ScriptableObject implements Function {
     // Inherited from ScriptableObject.
@@ -341,20 +302,4 @@ public abstract class GameUI extends RPCResponder {
     /** Run the callback. */
     public abstract Object run(Object[] args);
   }
-
-  /**
-   * Display a line of text to the user.  Called by the UI script.
-   */
-  public abstract void writeln(String line);
-
-  /**
-   * End the current game.  Called by the referee when the game ends.
-   */
-  public abstract void endGame();
-
-  /**
-   * Report a command error.
-   */
-  public abstract void error(Exception e);
-
 }
