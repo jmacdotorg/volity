5b180f79fa4e9deab9b020caacf92b8c78828093
Jason McIntosh <jmac@jmac.org>
2004-08-24 17:37:18 +0000

    Out-of-date files removed from CVS.

--- server-perl/bin/guess100_start.sh
+++
@@ -1,2 +0,0 @@
-./server.pl -u test -p foo -h localhost -g "Volity::Game::Guess100" -r guess100server
-
--- server-perl/bin/rps_start.sh
+++
@@ -1,2 +0,0 @@
-./server.pl -u test -p foo -h localhost -g "Volity::Game::RPS" -r rpsserver
-
