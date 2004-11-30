7ca64a58572bef0f9cd2e1c84fd06177ba479bc1
Jason McIntosh <jmac@jmac.org>
2004-11-30 01:56:47 +0000

    Updated disco stuff

--- bookkeeper-perl/lib/Volity/Bookkeeper.pm
+++ bookkeeper-perl/lib/Volity/Bookkeeper.pm
@@ -23,7 +23,7 @@ use strict;
 
 use base qw(Volity::Jabber);
 
-our $VERSION = '0.2';
+our $VERSION = '0.4';
 use DBIx::Abstract;
 use Carp qw(carp croak);
 
@@ -31,6 +31,7 @@ use Volity::GameRecord;
 use Volity::Info::Server;
 use Volity::Info::Game;
 use Volity::Info::Player;
+use Volity::Info::File;
 
 use POE qw(
 	   Wheel::SocketFactory
@@ -48,6 +49,11 @@ use POE qw(
 # methods, instead of elsif-ing through a long list of possible request
 # names, we will call only methods that begin with "_rpc_". This offers pretty
 # good security, I think.
+# If the request is doable (this method exists on the bookkeeper object), then
+# it captures the return list. If it has zero or one value, then it passes
+# this back to the caller as an RPC resposne. If it has two values, then it
+# passes it back as an RPC fault, using the first value as an error cose and
+# the second as an error string.
 sub handle_rpc_request {
   my $self = shift;
   my ($rpc_info) = @_;
@@ -56,12 +62,17 @@ sub handle_rpc_request {
     my $method = $1;
     $method = "_rpc_" . $method;
     if ($self->can($method)) {
-      $self->$method($$rpc_info{from}, $$rpc_info{id}, @{$$rpc_info{args}});
+      my @return_values = $self->$method($$rpc_info{from}, @{$$rpc_info{args}});
+      if (@return_values < 2) {
+	  $self->send_rpc_response($$rpc_info{from}, $$rpc_info{id}, @return_values);
+      } else {
+	  $self->send_rpc_fault($$rpc_info{from}, $$rpc_info{id}, @return_values);
+      }
     } else {
-      warn "I received a $$rpc_info{method} RPC request from $$rpc_info{from}, but I don't know what to do about it.\n";
+      $self->logger->warn("I received a $$rpc_info{method} RPC request from $$rpc_info{from}, but I don't know what to do about it.\n");
     }
   } else {
-    warn "Received a $$rpc_info{method} RPC request; it's not in the volity namespace, so I'm ignoring it.";
+    $self->logger->warn("Received a $$rpc_info{method} RPC request; it's not in the volity namespace, so I'm ignoring it.");
   }
 }
 
@@ -83,6 +94,7 @@ sub jabber_presence {
 
 sub handle_disco_info_request {
     my $self = shift;
+
     my ($iq) = @_;
     $self->logger->debug("I got a disco info request from " . $iq->attr('from'));
     # I'm making up my own category and type stuff, here.
@@ -94,16 +106,48 @@ sub handle_disco_info_request {
     my $identity = Volity::Jabber::Disco::Identity->new({category=>'volity',
 							 type=>'bookkeeper',
 						     });
-    unless ($iq->get_tag('query')->attr('node')) {
+    my %fields;			# JEP-1028 form fields to send back.
+    if (my $node_text = $iq->get_tag('query')->attr('node')) {
+	my @nodes = split(/\|/, $node_text);
+	if (my $file = $self->get_file_with_url($nodes[0])) {
+	    $identity->type('ui');
+	    my $ruleset_uri = $file->ruleset_id->uri;
+	    my @features = $file->features;
+	    my @languages = $file->languages;
+	    $fields{"client-type"} = [map($_->uri, @features)];
+	    $fields{languages} = [map($_->code, @languages)];
+	    $fields{ruleset} = [$ruleset_uri];
+	    $fields{reputation} = [$file->reputation || 0];
+	    $fields{"contact-email"} = [$file->player_id->email];
+	    $fields{"contact-jid"} = [$file->player_id->jid];
+	    $fields{description} = [$file->description];
+							
+	} elsif (my $server = $self->get_server_with_jid($nodes[0])) {
+	    $fields{reputation} = [$server->reputation || 0];
+	} elsif (my $ruleset = $self->get_ruleset_with_uri($nodes[0])) {
+	    $fields{description} = [$ruleset->description];
+	}
+    } else {
 	$identity->name('the volity.org bookkeeper');
     }
+    
     push (@items, $identity);
+
+    # Build the JEP-0128 form-field objects.
+    my @fields;
+    while (my($var, $values) = each(%fields)) {
+	my $field = Volity::Jabber::Form::Field->new({var=>$var});
+	$field->values(@$values);
+	push (@fields, $field);
+    }
 
     $self->send_disco_info({
 	to=>$iq->attr('from'),
 	id=>$iq->attr('id'),
 	items=>\@items,
+	fields=>\@fields,
     });
+
 }
 
 sub handle_disco_items_request {
@@ -113,7 +157,7 @@ sub handle_disco_items_request {
     my $query = $iq->get_tag('query');
     my @nodes;
     if (defined($query->attr('node'))) {
-	@nodes = split(/\//, $query->attr('node'));
+	@nodes = split(/\|/, $query->attr('node'));
     }
     
     my @items;			# Disco items to return to the requester.
@@ -121,77 +165,119 @@ sub handle_disco_items_request {
     # The logic in the following section is dirty and yukky coz I dunno
     # what I'm doing yet.
     if (@nodes) {
-	if ($nodes[0] eq 'games') {
-	    if (defined($nodes[1])) {
-		my $ruleset_name = $nodes[1];
-		my ($ruleset) = Volity::Info::Ruleset->search({name=>$ruleset_name});
-		if ($nodes[2]) {
-		    if ($nodes[2] eq 'servers') {
-			my @servers = Volity::Info::Server->search({
-			    ruleset_id=>$ruleset->id,
-			    });
-			for my $server (@servers) {
-			    push (@items, Volity::Jabber::Disco::Item->new({
-				jid=>$server->jid . "/volity",
-				name=>$server->jid,
-			    }),
-				  );
-			}
-		    } elsif ($nodes[2] eq 'uis') {
-			# Not really sure yet.
-			# Not sure if this should even be here.
-		    } else {
-			# There should be a disco-error here.
+	if ($nodes[0] eq 'rulesets') {
+	    # 'rulesets' node requested, but nothing else.
+	    # So let's return a list of all known rulesets.
+	    # NOTE: This isn't a scalable solution!!
+	    my @rulesets = Volity::Info::Ruleset->retrieve_all;
+	    for my $ruleset (@rulesets) {
+		push (@items, Volity::Jabber::Disco::Item->new({
+		    jid=>$self->jid,
+		    node=>$ruleset->uri,
+		    name=>$ruleset->description,
+		}),
+		      );
+	    }
+	} elsif (my $ruleset = $self->get_ruleset_with_uri($nodes[0])) {
+	    my $ruleset_uri = $ruleset->uri;
+	    if ($nodes[1]) {
+		if ($nodes[1] eq 'servers') {
+		    my @servers = Volity::Info::Server->search({
+			ruleset_id=>$ruleset->id,
+		    });
+		    for my $server (@servers) {
+			push (@items, Volity::Jabber::Disco::Item->new({
+#			    jid=>$server->jid . "/volity",
+			    jid=>$self->jid,
+			    node=>$server->jid,
+			    name=>$server->jid,
+			}),
+			      );
+		    }
+		} elsif ($nodes[1] eq 'uis') {
+		    my @files = Volity::Info::File->search({
+			ruleset_id=>$ruleset->id,
+		    });
+		    for my $file (@files) {
+			my $reputation = defined($file->reputation)?
+			    $file->reputation : 0;
+			my $pretty_name = "($reputation) " .
+			    $file->name;
+			$pretty_name .= ": " . $file->description
+			    if defined($file->description);
+			push (@items, Volity::Jabber::Disco::Item->new({
+			    node=>$file->url,
+			    jid=>$self->jid,
+			    name=>$pretty_name,
+			})
+			      );
 		    }
 		} else {
-		    push (@items, 
-			  Volity::Jabber::Disco::Item->new({
-			      jid=>$self->jid,
-			      node=>"games/$ruleset_name/servers",
-			      name=>"List of servers for this game",
-			  }),
-			  Volity::Jabber::Disco::Item->new({
-			      jid=>$self->jid,
-			      node=>"games/$ruleset_name/uis",
-			      name=>"List of uis for this game",
-			  }),
-			  );
+		    # There should be a disco-error here.
 		}
-	    } else {	
-		# 'games' node requested, but nothing else.
-		# So let's return a list of all known rulesets.
-		# NOTE: This isn't a scalable solution!!
-		my @rulesets = Volity::Info::Ruleset->retrieve_all;
-		for my $ruleset (@rulesets) {
-		    push (@items, Volity::Jabber::Disco::Item->new({
-			jid=>$self->jid,
-			node=>"games/" . $ruleset->name,
-			name=>$ruleset->description,
-		    }),
-			  );
-		}
+	    } else {
+		push (@items, 
+		      Volity::Jabber::Disco::Item->new({
+			  jid=>$self->jid,
+			  node=>"$ruleset_uri|servers",
+			  name=>"List of servers for this game",
+		      }),
+		      Volity::Jabber::Disco::Item->new({
+			  jid=>$self->jid,
+			  node=>"$ruleset_uri|uis",
+			  name=>"List of uis for this game",
+		      }),
+		      );
 	    }
-	} else {
-	    # Should be a disco-error here; we don't support nodes
-	    # other than 'games' right now.
+	} elsif (my $server = $self->get_server_with_jid($nodes[0])) {
+	    # For now, I'll just inlcude a pointer to the server,
+	    # as an item.
+	    push (@items, Volity::Jabber::Disco::Item->new({
+		jid=>$server->jid . "/volity",
+		name=>$server->jid,
+	    }));
+	} elsif (my $file = $self->get_file_with_url($nodes[0])) {
+	    # Actually, in this case, no further items are generated.
+	    # I'm leaving this inefficient elsif here to make things
+	    # more explicit for myself; may drop later.
 	}
-	    
+	
     } else {
 	# No nodes defined; this is a top-level request.
 	push (@items, Volity::Jabber::Disco::Item->new({
 	    jid=>$self->jid,
-	    node=>"games",
+	    node=>"rulesets",
 	    name=>"List of known Volity rulesets",
 	})
 	      );
     }
-    
+
     $self->send_disco_items({to=>$iq->attr('from'),
 			     id=>$iq->attr('id'),
 			     items=>\@items});
 }
 
+# get_file_from_url: return a file object from the given URI, or undef.
+sub get_file_with_url {
+    my $self = shift;
+    my ($url) = @_;
+    my ($file) = Volity::Info::File->search({url=>$url});
+    return $file;
+}
 
+sub get_ruleset_with_uri {
+    my $self = shift;
+    my ($uri) = @_;
+    my ($ruleset) = Volity::Info::Ruleset->search({uri=>$uri});
+    return $ruleset;
+}
+	
+sub get_server_with_jid {
+    my $self = shift;
+    my ($jid) = @_;
+    my ($server) = Volity::Info::Server->search({jid=>$jid});
+    return $server;
+}
 
 ####################
 # RPC methods
@@ -210,24 +296,24 @@ sub handle_disco_items_request {
 # the server, and then store the record in the DB.
 sub _rpc_record_game {
   my $self = shift;
-  my ($sender_jid, $rpc_id_attr, $game_record_hashref) = @_;
+  my ($sender_jid, $game_record_hashref) = @_;
   my $game_record = Volity::GameRecord->new_from_hashref($game_record_hashref);
   unless (defined($game_record)) {
-    warn "Got bad game struct from $sender_jid.\n";
+    $self->logger->warn("Got bad game struct from $sender_jid.\n");
     # XXX Error response here.
-    return;
+    return ('999', 'Bad game struct.');
   }
 
   # Verify the signature!
   unless ($game_record->verify) {
-    warn "Uh oh... signature on game record doesn't seem to verify!!\n";
+    $self->logger->warn("Uh oh... signature on game record doesn't seem to verify!!\n");
     # XXX Error response here.
-    return;
+    return ('999', 'Bad signature.');
   }
 
   # Looks good. Store it.
   $self->store_record_in_db($game_record);
-  
+  return ('ok');
 }
 
 sub store_record_in_db {
@@ -235,12 +321,12 @@ sub store_record_in_db {
   my ($game_record) = @_;
   my ($server) = Volity::Info::Server->search({jid=>$game_record->server});
   unless ($server) {
-      warn "Bizarre... got a record with server JID " . $game_record->server . ", but couldn't get a server object from the DB from it. No record stored.";
+      $self->logger->warn("Bizarre... got a record with server JID " . $game_record->server . ", but couldn't get a server object from the DB from it. No record stored.");
       return;
   }
   my ($ruleset) = Volity::Info::Ruleset->search({uri=>$game_record->game_uri});
   unless ($ruleset) {
-      warn "Bizarre... got a record with server JID " . $game_record->game_uri . ", but couldn't get a server object from the DB from it. No record stored.";
+      $self->logger->warn("Bizarre... got a record with server JID " . $game_record->game_uri . ", but couldn't get a server object from the DB from it. No record stored.");
       return;
   }
   my $game;			# Volity::Info::Game object
@@ -266,7 +352,6 @@ sub store_record_in_db {
     foreach ($class->search({game_id=>$game->id})) {
       $_->delete;
     }
-#    my @players = map(Volity::Info::Player->search({jid=>$_}),
     my @players = map(Volity::Info::Player->find_or_create({jid=>$_}),
 		      grep(defined($_), $game_record->$player_list));
     for my $player (@players) {
@@ -294,7 +379,6 @@ sub store_record_in_db {
 
       # Figure out the player's new ranking for this game.
       my $last_rating = $player->current_rating_for_ruleset($ruleset);
-#      warn "***Da last rating for $player and $ruleset is $last_rating. de END.";
       my @beaten_players; my @tied_players; # Volity::Info::Player objects.
       my @winning_players;	            # Ibid.
       # Get the players that beat this one.
@@ -358,9 +442,17 @@ sub get_rating_delta {
   return 1 / (1 + (10 ^ (abs($current_rating - $opponent_rating) / 400)));
 }
 
+###############################
+# CAUTION
+###############################
+# All the following methods (up until the POE stuff) are nonfunctional.
+# I'm keeping them in the code because they _used_ to work, with my
+# older DB model, and I want to rewrite them sometime.
+###############################
+
 sub _rpc_set_my_attitude_toward_player {
   my $self = shift;
-  my ($sender_jid, $id, $target_jid, $rank) = @_;
+  my ($sender_jid, $target_jid, $rank) = @_;
   $rank = int($rank);
   if ($rank < -1 or $rank > 1) {
     # XXX Error!
@@ -374,18 +466,18 @@ sub _rpc_set_my_attitude_toward_player {
 
 sub _rpc_get_my_attitude_toward_player {
   my $self = shift;
-  my ($sender, $id, $target) = @_;
+  my ($sender, $target) = @_;
   my $dbh = $self->dbh;
   $dbh->select('ATTITUDE', 'PLAYER_ATTITUDE', {FROM_JID=>$sender,
 					       TO_JID=>$target});
   my ($att) = $dbh->fetchrow_array;
-  $self->send_rpc_response($sender, $id, $att);
+#  $self->send_rpc_response($sender, $id, $att);
 }
 
 # This method returns three scalars, with counts of -1, 0, and 1.
 sub _rpc_get_all_attitudes_toward_player {
   my $self = shift;
-  my ($sender, $id, $target) = @_;
+  my ($sender, $target) = @_;
   my $dbh = $self->dbh;
   $dbh->select('ATTITUDE', 'PLAYER_ATTITUDE', {FROM_JID=>$sender,
 					       TO_JID=>$target});
@@ -394,23 +486,23 @@ sub _rpc_get_all_attitudes_toward_player {
     $atts[$att + 1]++;
   }
 
-  $self->send_rpc_response($sender, $id, \@atts);
+#  $self->send_rpc_response($sender, $id, \@atts);
 }
 
 sub _rpc_get_all_game_records_for_player {
   my $self = shift;
-  my ($sender_jid, $rpc_id_attr, $player_jid) = @_;
+  my ($sender_jid, $player_jid) = @_;
   my @game_records = $self->fetch_game_records_for_player($player_jid);
-  $self->send_rpc_response($sender_jid, $rpc_id_attr,
-			   [map($_->render_as_hashref, @game_records)]);
+#  $self->send_rpc_response($sender_jid, $rpc_id_attr,
+#			   [map($_->render_as_hashref, @game_records)]);
 }
 
 sub _rpc_get_game_records_for_player_and_game {
   my $self = shift;
-  my ($sender_jid, $rpc_id_attr, $player_jid, $game_uri) = @_;
+  my ($sender_jid, $player_jid, $game_uri) = @_;
   my @game_records = $self->fetch_game_records_for_player($player_jid, {GAME_URI=>$game_uri});
-  $self->send_rpc_response($sender_jid, $rpc_id_attr,
-			   [map($_->render_as_hashref, @game_records)]);
+#  $self->send_rpc_response($sender_jid, $rpc_id_attr,
+#			   [map($_->render_as_hashref, @game_records)]);
 }
 
 sub fetch_game_records_for_player {
@@ -431,31 +523,31 @@ sub fetch_game_records_for_player {
 
 sub _rpc_get_game_records_for_player_and_server {
   my $self = shift;
-  my ($sender_jid, $rpc_id_attr, $player_jid, $server_jid) = @_;
+  my ($sender_jid, $player_jid, $server_jid) = @_;
   my @game_records = $self->fetch_game_records_for_player($player_jid, {SERVER_JID=>$server_jid});
-  $self->send_rpc_response($sender_jid, $rpc_id_attr,
-			   [map($_->render_as_hashref, @game_records)]);
+#  $self->send_rpc_response($sender_jid, $rpc_id_attr,
+#			   [map($_->render_as_hashref, @game_records)]);
 }
   
 sub _rpc_get_all_totals_for_player {
   my $self = shift;
-  my ($sender, $rpc_id, $player_jid) = @_;
+  my ($sender, $player_jid) = @_;
   my %totals = $self->fetch_totals_for_player($player_jid);
-  $self->send_rpc_response($sender, $rpc_id, \%totals);
+#  $self->send_rpc_response($sender, $rpc_id, \%totals);
 }
 
 sub _rpc_get_all_totals_for_player_and_game {
   my $self = shift;
   my ($sender, $rpc_id, $player_jid, $game_uri) = @_;
   my %totals = $self->fetch_totals_for_player($player_jid, {GAME_URI=>$game_uri});
-  $self->send_rpc_response($sender, $rpc_id, \%totals);
+#  $self->send_rpc_response($sender, $rpc_id, \%totals);
 }
 
 sub _rpc_get_all_totals_for_player_and_server {
   my $self = shift;
-  my ($sender, $rpc_id, $player_jid, $server_jid) = @_;
+  my ($sender, $player_jid, $server_jid) = @_;
   my %totals = $self->fetch_totals_for_player($player_jid, {SERVER_JID=>$server_jid});
-  $self->send_rpc_response($sender, $rpc_id, \%totals);
+#  $self->send_rpc_response($sender, $rpc_id, \%totals);
 }
   
 sub fetch_totals_for_player {
@@ -489,6 +581,24 @@ sub fetch_totals_for_player {
   return %totals;
 }
 
+sub _rpc_get_urls_for_ruleset_and_client_type {
+    my $self = shift;
+    my ($sender, $ruleset_ui, $client_type) = @_;
+    my ($ruleset) = Volity::Info::Ruleset->search({uri=>$ruleset_ui, type=>$client_type});
+    my @files = Volity::Info::File->search({ruleset_id=>$ruleset});
+    my @return_value;
+    for my $file (@files) {
+	my %info = (
+		    url=>$file->url,
+		    rating=>$file->rating || 0,
+		    name=>$file->name,
+		    description=>$file->description,
+		    );
+	push (@return_value, \%info);
+    }
+    return \@return_value;
+}
+
 ####################
 # POE stuff
 ####################
@@ -507,27 +617,6 @@ sub jabber_authed {
   unless ($node->name eq 'handshake') {
 #    warn $node->to_str;
   }
-}
-
-########################
-# Disco Stuff (I think??)
-########################
-
-# These methods cover stuff you'd find through service discovery (JEP-0030).
-
-# servers_of_game: For a given game URI, return the JIDs of the servers that
-# provide it.
-sub servers_of_game {
-  my $self = shift;
-  my $dbh = $self->dbh;
-  my ($game_uri) = @_;
-  $dbh->select('JID', 'SERVER', {GAME_URI=>$game_uri});
-  my @server_jids;
-  while (my($jid) = $dbh->fetchrow_array) {
-    push (@server_jids, $jid);
-  }
-
-  return @server_jids;
 }
 
 1;
