104c9c196fc4e3315193cf76e3c0fbaf42afa79c
Andrew Plotkin <erkyrath@eblong.com>
2005-07-21 20:17:17 +0000

    Initial revision

---
+++ games-python/games/rps.py
@@ -0,0 +1,149 @@
+"""rps -- an implementation of Rock Paper Scissors
+Ruleset URI: <http://volity.org/games/rps>
+
+This is my implementation of Rock Paper Scissors.
+"""
+
+from zymb import jabber
+import zymb.jabber.rpc
+import volity.game
+
+class RPS(volity.game.Game):
+
+    gamename = 'Zarf\'s RPS'
+    gamedescription = 'An unofficial Volity.net RPS implementation, by Andrew Plotkin.'
+    ruleseturi = 'http://volity.org/games/rps' ### doesn't actually follow the ruleset at this address, because the ruleset is out of date.
+    rulesetversion = '1.0'
+    implementoremail = 'erkyrath@eblong.com'
+    implementorjid = 'zarf@jabber.org'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('no_ties', 'best_of', afoot=False)
+        self.validatecalls('choose_hand', afoot=True)
+        self.validatecalls('no_ties', args=int)
+        self.validatecalls('best_of', args=int)
+        self.validatecalls('choose_hand', args=str, seated=True)
+
+        self.no_ties = 1
+        self.best_of = 1
+        
+        self.whiteseat = RPSSeat(self, 'white')
+        self.blackseat = RPSSeat(self, 'black')
+
+    def begingame(self):
+        self.gamecount = 0
+        self.whiteseat.hand = None
+        self.whiteseat.wins = 0
+        self.blackseat.hand = None
+        self.blackseat.wins = 0
+
+    def sendconfigstate(self, player):
+        player.send('no_ties', '', self.no_ties)
+        player.send('best_of', '', self.best_of)
+
+    # The following methods are RPC handlers.
+                
+    def rpc_no_ties(self, sender, *args):
+
+        val = args[0]
+        if (not val in [0,1]):
+            raise jabber.rpc.RPCFault(606, 'no_ties must be 0 or 1')
+
+        if (val == self.no_ties):
+            return
+
+        self.no_ties = val
+
+        self.sendtable('no_ties', sender, self.no_ties)
+        self.unready()
+
+    def rpc_best_of(self, sender, *args):
+
+        val = args[0]
+        if (val <= 0 or (val & 1) == 0):
+            raise jabber.rpc.RPCFault(606, 'best_of must be positive and odd')
+
+        if (val == self.best_of):
+            return
+
+        self.best_of = val
+
+        self.sendtable('best_of', sender, self.best_of)
+        self.unready()
+
+    def rpc_choose_hand(self, sender, *args):
+
+        val = args[0]
+
+        if (not val in ['rock', 'paper', 'scissors']):
+            raise jabber.rpc.RPCFault(606, 'choose_hand must be "rock", "paper", or "scissors"')
+
+        seat = self.getplayerseat(sender)
+        if (not seat):
+            # the validator should prevent this from happening
+            raise ValueError('sender %s is not playing' % unicode(sender))
+
+        seat.hand = val
+        self.queueaction(self.checkoutcome)
+
+    def checkoutcome(self):
+        white = self.whiteseat
+        black = self.blackseat
+
+        if ((not white.hand) or (not black.hand)):
+            # no outcome yet
+            return
+                
+        self.sendtable('player_chose_hand', white.id, white.hand)
+        self.sendtable('player_chose_hand', black.id, black.hand)
+
+        if (white.hand == black.hand):
+            # tie
+            if (self.no_ties):
+                self.log.info('players tied, rethrowing')
+                white.hand = None
+                black.hand = None
+                return
+            self.log.info('both players won')
+            black.wins += 1
+            white.wins += 1
+        else:
+            if ((black.hand == 'rock' and white.hand == 'scissors')
+                or (black.hand == 'scissors' and white.hand == 'paper')
+                or (black.hand == 'paper' and white.hand == 'rock')):
+                winner = black
+            else:
+                winner = white
+            winner.wins += 1
+            self.log.info(winner.id + ' won')
+
+        self.gamecount += 1
+        white.hand = None
+        black.hand = None
+
+        if (self.gamecount < self.best_of):
+            return
+
+        if (black.wins > white.wins):
+            winner = black
+            self.log.info('black wins the match')
+        elif (black.wins < white.wins):
+            winner = white
+            self.log.info('white wins the match')
+        else:
+            winner = None
+            self.log.info('the match is a tie')
+
+        self.gameover(winner)
+
+    
+class RPSSeat(volity.game.Seat):
+    def __init__(self, game, id):
+        volity.game.Seat.__init__(self, game, id)
+        self.hand = None
+        self.wins = 0
+
+
---
+++ games-python/games/seattest.py
@@ -0,0 +1,208 @@
+"""seattest -- a collection of Volity non-games.
+
+These "games" exist only to exercise the Volity client for various
+seating arrangements. Once the game starts, they only support one
+RPC: "game.win", which a client sends in order to win the game.
+"""
+
+from zymb import jabber
+import zymb.jabber.rpc
+import volity.game
+
+# Games that allow "any number of players" will still have a maximum
+# number of seats. Twenty or thirty is a reasonable limit; a very social
+# game can use a higher limit.
+#
+# However, this module uses an unnaturally low limit, to make client 
+# testing easier. To wit: eight.
+#
+MAXPLAYERS = 8
+
+class TwoPlayer(volity.game.Game):
+    gamename = 'Seat-testing: two players'
+    description = 'Exactly two seats, must be filled'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/TwoPlayer.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+        
+        volity.game.Seat(self, 'white')
+        volity.game.Seat(self, 'black')
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+class FourPlayer(volity.game.Game):
+    gamename = 'Seat-testing: four players'
+    description = 'Exactly four seats, must be filled'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/FourPlayer.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+        
+        volity.game.Seat(self, 'north')
+        volity.game.Seat(self, 'south')
+        volity.game.Seat(self, 'east')
+        volity.game.Seat(self, 'west')
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+class AnyNumber(volity.game.Game):
+    gamename = 'Seat-testing: any number of players'
+    description = 'Any number of seats, at least one must be filled'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/AnyNumber.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+        
+        for ix in range(MAXPLAYERS):
+            volity.game.Seat(self, 'seat'+str(ix), False)
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+class TwoPlusAnyNumber(volity.game.Game):
+    gamename = 'Seat-testing: any number of players, including two captains'
+    description = 'Two captains, and any number of further seats'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/TwoPlusAnyNumber.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+
+        volity.game.Seat(self, 'captain0')
+        volity.game.Seat(self, 'captain1')
+        for ix in range(MAXPLAYERS):
+            volity.game.Seat(self, 'seat'+str(ix), False)
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+class TwoOrMore(volity.game.Game):
+    gamename = 'Seat-testing: two or more players'
+    description = 'Any number of seats, at least two must be filled'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/TwoOrMore.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+        
+        for ix in range(MAXPLAYERS):
+            volity.game.Seat(self, 'seat'+str(ix), False)
+
+    def checkseating(self):
+        ls = [ seat for seat in self.getseatlist()
+            if not seat.isempty() ]
+        if (len(ls) < 2):
+            raise volity.game.FailureToken('game.two_seats_needed')
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+class AnyNumberPairs(volity.game.Game):
+    gamename = 'Seat-testing: even number of players, seated in pairs'
+    description = 'Any even number of players, seated in pairs'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/AnyNumberPairs.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+        
+        for ix in range(MAXPLAYERS/2):
+            seatn = volity.game.Seat(self, 'seatnorth'+str(ix), False)
+            seats = volity.game.Seat(self, 'seatsouth'+str(ix), False)
+            seatn.partner = seats
+            seats.partner = seatn
+
+    def requestanyseat(self, player):
+        ls = [ seat for seat in self.getseatlist()
+            if (not seat.isempty()) and seat.partner.isempty() ]
+        if (ls):
+            return ls[0].partner
+        
+        ls = [ seat for seat in self.getseatlist()
+            if seat.isempty() ]
+        if (ls):
+            return ls[0]
+        return None
+    
+    def checkseating(self):
+        ls = [ seat for seat in self.getseatlist()
+            if (not seat.isempty()) and seat.partner.isempty() ]
+        if (ls):
+            raise volity.game.FailureToken('game.seat_pairs')
+
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+    
+
+class ConfigSeatCount(volity.game.Game):
+    gamename = 'Seat-testing: configurable number of seats'
+    description = 'A number of seats configured by the game'
+    ruleseturi = 'http://eblong.com/zarf/volity/ruleset/seattest/ConfigSeatCount.html'
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+
+        self.setopset(self)
+        self.validatecalls('config_count', afoot=False, args=int)
+        self.validatecalls('win', afoot=True, seated=True, argcount=0)
+
+        self.count = 2
+        
+        for ix in range(MAXPLAYERS):
+            volity.game.Seat(self, 'seat'+str(ix), False)
+            
+        ls = self.getseatlist()
+        self.setseatsrequired(ls[:self.count])
+
+    def requestparticularseat(self, player, seat):
+        ls = self.getseatlist()
+        legal = ls[:self.count]
+        if (not seat in legal):
+            raise volity.game.FailureToken('game.illegal_seat',
+                seat, self.count)
+            
+    def requestanyseat(self, player):
+        ls = [ seat for seat in self.getseatlist()
+            if (seat.isrequired() and seat.isempty()) ]
+        if (ls):
+            return ls[0]
+        return None
+
+    def rpc_config_count(self, sender, *args):
+        val = args[0]
+        if (val < 1 or val > MAXPLAYERS):
+            raise volity.game.FailureToken('game.config_count_bounds',
+                MAXPLAYERS)
+
+        ls = self.getseatlist()
+        subls = [ seat for seat in ls[val:]
+            if not seat.isempty() ]
+        if (subls):
+            raise volity.game.FailureToken('game.del_occupied_seat')
+
+        self.count = val
+        self.unready()
+        self.setseatsrequired(ls[:self.count], ls[self.count:])
+    
+    def rpc_win(self, sender, *args):
+        self.gameover(self.getplayerseat(sender))
+
+    
