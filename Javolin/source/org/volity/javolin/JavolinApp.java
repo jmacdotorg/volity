3306e35fe30833e438d1805cbbacde95140af69a
Andrew Plotkin <erkyrath@eblong.com>
2006-01-07 20:52:54 +0000

    Bumped version number in JavolinApp and CHANGELOG.

--- Javolin/source/org/volity/javolin/JavolinApp.java
+++ Javolin/source/org/volity/javolin/JavolinApp.java
@@ -53,7 +53,7 @@ public class JavolinApp extends JFrame
                RosterPanelListener, InvitationListener
 {
     private final static String APPNAME = "Javolin";
-    private final static String APPVERSION = "0.2.2";
+    private final static String APPVERSION = "0.2.3";
 
     private final static String NODENAME = "MainAppWin";
 
