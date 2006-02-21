c2981df0bb82f1d50df1f739db9b7961afd182c8
Jason McIntosh <jmac@jmac.org>
2006-02-21 23:22:13 +0000

    0.2.7 bumps

--- Javolin/source/org/volity/javolin/JavolinApp.java
+++ Javolin/source/org/volity/javolin/JavolinApp.java
@@ -53,7 +53,7 @@ public class JavolinApp extends JFrame
                RosterPanelListener, InvitationListener
 {
     private final static String APPNAME = "Javolin";
-    private final static String APPVERSION = "0.2.6";
+    private final static String APPVERSION = "0.2.7";
 
     private final static String NODENAME = "MainAppWin";
 
