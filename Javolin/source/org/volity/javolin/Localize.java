b44e9ca5631351bc28764b8085b5bef76b285715
Andrew Plotkin <erkyrath@eblong.com>
2006-07-25 16:52:16 +0000

    Whoops, forgot to check this in.

---
+++ Javolin/source/org/volity/javolin/Localize.java
@@ -0,0 +1,56 @@
+package org.volity.javolin;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+
+/**
+ * Simple class for getting localized text from a resource file. This contains
+ * only static methods.
+ */
+public class Localize 
+{
+    private Localize() {
+        // Do not instantiate.
+    }
+
+    /**
+     * Get a key from a resource file.
+     */
+    public static String localize(String node, String key) {
+        try {
+            return JavolinApp.resources.getString(node+"_"+key);
+        }
+        catch (MissingResourceException ex) {
+            return "???"+node+"_"+key;
+        }
+    }
+
+    /**
+     * Get a key from a resource file, interpolating one string. (The argument
+     * string must already be localized.)
+     */
+    public static String localize(String node, String key, Object arg1) {
+        try {
+            String pattern = JavolinApp.resources.getString(node+"_"+key);
+            return MessageFormat.format(pattern, new Object[] { arg1 });
+        }
+        catch (MissingResourceException ex) {
+            return "???"+node+"_"+key;
+        }
+    }
+
+    /**
+     * Get a key from a resource file, interpolating two strings. (The argument
+     * strings must already be localized.)
+     */
+    public static String localize(String node, String key, Object arg1, Object arg2) {
+        try {
+            String pattern = JavolinApp.resources.getString(node+"_"+key);
+            return MessageFormat.format(pattern, new Object[] { arg1, arg2 });
+        }
+        catch (MissingResourceException ex) {
+            return "???"+node+"_"+key;
+        }
+    }
+
+}
